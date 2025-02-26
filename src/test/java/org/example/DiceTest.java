c18c3d0018e76dd4040712194323971faaca40b6
vikko <vikkov@metropolia.fi>
2025-02-26 17:36:28 +0200

    added more tests

--- src/test/java/org/example/DiceTest.java
+++ src/test/java/org/example/DiceTest.java
@@ -17,13 +17,53 @@ class DiceTest {
     @org.junit.jupiter.api.Test
     void isDouble() {
         Dice dice = new Dice();
-        dice.roll();
         int die1 = dice.getDie1();
         int die2 = dice.getDie2();
-        if (die1 == die2) {
-            assertEquals(true, dice.isDouble());
-        } else {
-            assertEquals(false, dice.isDouble());
+        while (die1 != die2) {
+            dice.roll();
+            die1 = dice.getDie1();
+            die2 = dice.getDie2();
+        }
+        assertTrue(dice.isDouble());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDie1() {
+        Dice dice = new Dice();
+        dice.roll();
+        int die1 = dice.getDie1();
+        if (die1 == 1) {
+            assertEquals(1, die1);
+        } else if (die1 == 2) {
+            assertEquals(2, die1);
+        } else if (die1 == 3) {
+            assertEquals(3, die1);
+        } else if (die1 == 4) {
+            assertEquals(4, die1);
+        } else if (die1 == 5) {
+            assertEquals(5, die1);
+        } else if (die1 == 6) {
+            assertEquals(6, die1);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDie2() {
+        Dice dice = new Dice();
+        dice.roll();
+        int die2 = dice.getDie2();
+        if (die2 == 1) {
+            assertEquals(1, die2);
+        } else if (die2 == 2) {
+            assertEquals(2, die2);
+        } else if (die2 == 3) {
+            assertEquals(3, die2);
+        } else if (die2 == 4) {
+            assertEquals(4, die2);
+        } else if (die2 == 5) {
+            assertEquals(5, die2);
+        } else if (die2 == 6) {
+            assertEquals(6, die2);
         }
     }
 }
